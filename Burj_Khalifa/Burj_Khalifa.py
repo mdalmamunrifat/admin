55aa0891828066ec1067cd5d0428d13a141ff89e
Md. Al Mamun Rifat Joy <99207213+mdalmamunrifat@users.noreply.github.com>
2022-02-08 23:06:12 +0600

    Create Burj_Khalifa.py

---
+++ Burj_Khalifa/Burj_Khalifa.py
@@ -0,0 +1,19 @@
+print("Program for Burj Khalifa")
+
+print("=========================================================")
+
+n = int(input("Please enter Raw number: "))
+
+def startPrintingFunction(total, row):
+    for raw in range(total):
+        for new in range(total-row):
+            print("  ", end="")
+        for coloum in range(1, 2*row):
+            print("*", end=" ")
+        print()
+for num in range(1, n):
+    startPrintingFunction(n, num)
+print("End Now")
+    
+
+
